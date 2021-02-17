3db2d009142c9cff6a3f7afe841a151f405a458b
Gk0Wk <nmg_wk@yeah.net>
2021-02-17 13:53:47 +0800

    修改查询地址

--- assets/js/credit.js
+++ assets/js/credit.js
@@ -1,9 +1,11 @@
+var base = 'http://light.icursors.net:8816';
+
 /**
  * 调用牛腩信用分查询
  * @param {obj} params {type: 查询类型(name, qq, discord), value: 查询值, success: 成功时的handle(response => {}), failed: 失败时的bandle(err => {})}
  */
 module.exports.check = function check(params) {
-    fetch('https://cht.icursors.net/newnan_credit/check', {
+    fetch(base + '/newnan_credit/check', {
             method : 'POST',
             headers: {
                 'Content-Type': 'application/json'
